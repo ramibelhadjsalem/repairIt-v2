bf2a9427a49dd9ba70d20f60ff4a1a0dc2c1472b
ramibelhadjsalem <60022426+ramibelhadjsalem@users.noreply.github.com>
2023-02-04 17:45:16 +0100

    includs global scss variables

--- src/app/reparation/address/address.component.ts
+++ src/app/reparation/address/address.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { MapGeocoder } from '@angular/google-maps';
+import { GoogleMap, MapDirectionsService, MapGeocoder } from '@angular/google-maps';
+import { map, Observable } from 'rxjs';
 import { RepartionService } from 'src/app/services/services/repartion.service';
 
 @Component({
@@ -14,13 +15,20 @@ export class AddressComponent implements OnInit{
   zoom = 4;
   markerOptions: google.maps.MarkerOptions = {draggable: false};
   myposition ?:google.maps.LatLngLiteral
-
+  
+  
   form !:FormGroup
-  constructor(private formService :RepartionService,private geocoder: MapGeocoder) { }
+ 
+  constructor(private formService :RepartionService,private geocoder: MapGeocoder) {}
  
 
   ngOnInit(): void {
     this.initform()
+
+    setTimeout(()=>{
+      this.navigateTo()
+    },3000)
+    
   }
   initform(){
     this.form = this.formService.form.get('address') as FormGroup
@@ -30,8 +38,14 @@ export class AddressComponent implements OnInit{
     if(event.latLng !=null){
       this.myposition = event.latLng.toJSON()
       this.form.setValue(event.latLng.toJSON())
+     
     } 
   }
+  navigateTo(){
+    // this.maps.panTo({lat: 50, lng: 60})
+  }
+ 
+  
 
   
  
