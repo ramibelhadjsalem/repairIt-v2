36203d33fbeec564f137b37e6290c7259cb8b93f
ramibelhadjsalem <60022426+ramibelhadjsalem@users.noreply.github.com>
2023-02-22 15:51:51 +0100

    profile update

--- src/app/main/profile-id/profile-id.component.ts
+++ src/app/main/profile-id/profile-id.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile-id',
   templateUrl: './profile-id.component.html',
   styleUrls: ['./profile-id.component.scss']
 })
-export class ProfileIdComponent {
-
+export class ProfileIdComponent implements OnInit {
+  tab= "about"
+  constructor(private route:ActivatedRoute){}
+  ngOnInit() {
+    this.route.queryParams
+      .subscribe(params => {
+        const param =params['tab']
+        if(param) this.tab =param;
+        console.log(this.tab); // price
+      }
+    );
+  }
 }
